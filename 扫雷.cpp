8697ce7815b6117e0a83ace4a03e632996d6e875
2753910306 <87286181+2753910306@users.noreply.github.com>
2021-07-24 10:27:09 +0800

    Add files via upload

--- 扫雷.cpp
+++ 扫雷.cpp
@@ -2,7 +2,57 @@
 
 #include"game.h"
 
+//排雷
+int get_mine_count(char mine[ROWS][COLS], int x, int y)
+{
+	return mine[x - 1][y]
+		+ mine[x - 1][y - 1]
+		+ mine[x - 1][y + 1]
+		+ mine[x][y - 1]
+		+ mine[x + 1][y - 1]
+		+ mine[x][y + 1]
+		+ mine[x + 1][y]
+		+ mine[x + 1][y + 1] - 8 * '0';
+}
 
+void Findmine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	int win = 0;
+	
+	while (win < row*col-EASY_count)
+	{
+		printf("请输入坐标：");
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (mine[x][y] == '1')
+			{
+				printf("很遗憾，你被炸死了\n");
+				DispalyBoard(mine, row, col);
+				break;
+			}
+			else//不是雷
+			{
+				//计算（x，y）周围的雷
+				int count = get_mine_count(mine, x, y);
+				show[x][y] = count + '0';
+				DispalyBoard(show, row, col);
+				win++;
+			}
+		}
+		else
+		{
+			printf("输入坐标非法，请重新输入！\n");
+		}
+	}
+	if (win == row * col - EASY_count)
+	{
+		printf("恭喜你排雷成功!\n");
+		DispalyBoard(mine, row, col);
+	}
+}
 void Settine(char board[ROWS][COLS], int row, int col)
 {
 	int count = EASY_count;
@@ -21,8 +71,15 @@ void DispalyBoard(char board[ROWS][COLS], int row, int col)
 {
 	int i = 0;
 	int j = 0;
-	for (i = 1; i <= row; i++)
+	//打印列号
+	for (i = 0; i <= col; i++)
 	{
+		printf("%d ", i);
+	}
+	printf("\n");
+	for (i = 1; i <=row; i++)
+	{
+		printf("%d ", i);
 		for (j = 1; j <= col; j++)
 		{
 			printf("%c ", board[i][j]);
@@ -34,10 +91,10 @@ void InitBoard(char board[ROWS][COLS], int rows, int cols,char set)
 {
 	int i = 0;
 	int j = 0;
-	for (i = 0; i < rows; i++)
+	for (i = 0; i <= rows+1; i++)
 	{
 		
-		for (j = 0; j < cols;j++)
+		for (j = 0; j <= cols+1;j++)
 		{
 			board[i][j] = set;
 		}
@@ -50,12 +107,16 @@ void game()
 	char mine[ROWS][COLS] = { 0 };
 	char show[ROWS][COLS] = { 0 };
 	InitBoard(mine, ROW, COL, '0');
-	InitBoard(mine, ROW, COL, '*');
+	InitBoard(show, ROW, COL, '*');
 	//打印棋盘
-	DispalyBoard(mine, ROW, COL);
-	DispalyBoard(mine, ROW, COL);
+	DispalyBoard(show, ROW, COL);
 	//布置雷
 	Settine(mine, ROW, COL);
+	//扫雷
+	Findmine(mine,show,ROW,COL);
+	
+
+	
 }
 
 void menu()
@@ -73,7 +134,7 @@ void test()
 	{
 		
 		menu();
-		printf("请选择：");
+		printf("1 >玩游戏\n0 >退出游戏\n请选择 :");
 		scanf("%d", &input);
 		switch (input)
 		{
@@ -87,7 +148,7 @@ void test()
 			printf("选择错误，请重新选择!\n");
 			break;
 		}
-	} while (1);
+	} while (input);
 }
 
 int main()
