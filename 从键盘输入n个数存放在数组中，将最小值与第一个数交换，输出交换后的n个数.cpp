9d794fb088c68cbe16bb1f9bde368a23f7030a80
2753910306 <87286181+2753910306@users.noreply.github.com>
2021-07-25 16:13:11 +0800

    Add files via upload

---
+++ 从键盘输入n个数存放在数组中，将最小值与第一个数交换，输出交换后的n个数.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int i, n, iIndex, temp;
+
+    int a[10];
+
+    printf("Enter n: ");
+
+    scanf("%d", &n);
+
+    printf("Enter %d integers:\n ");
+
+    for (i = 0; i < n; i++)
+
+        scanf("%d", &a[i]);
+
+    iIndex = 0;
+
+    for (i = 1; i < n; i++) {
+
+        if (a[i] < a[iIndex])       iIndex = i;
+
+    }
+
+    temp = a[0]; a[0] = a[iIndex]; a[iIndex] = temp;
+
+    for (i = 0; i < n; i++)
+
+        printf("%5d", a[i]);
+
+    printf("\n");
+
+    return 0;
+
+}
