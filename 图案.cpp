50d9f8e17f07dbd7496a91887a4087eaaf33337a
2753910306 <87286181+2753910306@users.noreply.github.com>
2021-07-25 15:18:48 +0800

    Add files via upload

---
+++ 图案.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int i, f[10];
+
+    f[0] = f[1] = 1;
+
+    for (i = 2; i < 10; i++)
+
+        f[i] = f[i - 2] + f[i - 1];
+
+    for (i = 0; i < 10; i++)
+
+    {
+        if (i % 4 == 0)
+
+            printf("\n");
+
+        printf("%d ", f[i]);
+
+    }
+
+}
+
