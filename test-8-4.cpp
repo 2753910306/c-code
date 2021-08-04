7be09a4266621bf7cbd2cebd688514be573c6c28
2753910306 <87286181+2753910306@users.noreply.github.com>
2021-08-04 12:18:11 +0800

    Add files via upload

---
+++ test-8-4.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "abcdef";
+	char* p1 = "abcdef";
+	char *p2 = "abcdef";
+	if (p1 == p2)
+	{
+		printf("hehe\n");
+	}
+	else
+		printf("haha\n");
+	return 0;
+}
