ef41804a3fcfe53474e77dada900158f78ebbfb5
2753910306 <87286181+2753910306@users.noreply.github.com>
2021-08-11 17:05:11 +0800

    Add files via upload

--- 编写fun函数，把x值插入到 有序数组a中，插入后，数组a 中的数仍然有序.cpp
+++ 编写fun函数，把x值插入到 有序数组a中，插入后，数组a 中的数仍然有序.cpp
@@ -1,32 +1,37 @@
 #include<stdio.h>
+#define N 11
 
-void fun(int* a,int x,int sz)
+int fun(int* a,int x)
 {
 	int i;
 	int t;
-	*(a+sz) = x;
-	for (i = 0; i < sz; i++)
+	int n = 0;
+	*(a+10) = x;
+	for (i = 0; i < 10; i++)
 	{
 		if (x < *(a+i))
 		{
-			t = *(a+sz);
-			*(a+sz) = *(a+i);
+			t = *(a+10);
+			*(a+10) = *(a+i);
 			*(a+i) = t;
 		}
 		
+		
 	}
+	return n;
 }
 int main()
 {
-	int a[] = { 1,3,5,7,15,32,34,61,72,80 };
-	int x,sz=0;
+	int a[N] = { 1,3,5,7,15,32,34,61,72,80 };
+	int x;
 	int i=0;
-	sz = sizeof(a) / sizeof(a[0]);
+	int n = 0;
+	
 	printf("请输入你想插入的数字：\n");
-	scanf("%d", &x);
-	fun(a, x, sz);
-	printf("排序后为：");
-	for (i = 0; i <= sz; i++)
+	scanf_s("%d", &x);
+	fun(a, x);
+	printf("插入并排序后为：");
+	for (i = 0; i <N; i++)
 	{
 		printf("%d ", a[i]);
 	}
