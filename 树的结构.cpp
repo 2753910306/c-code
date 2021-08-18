e2d966bb651b847fe964ba78834c8c7304799790
2753910306 <87286181+2753910306@users.noreply.github.com>
2021-08-18 17:04:09 +0800

    Add files via upload

---
+++ 树的结构.cpp
@@ -0,0 +1,29 @@
+//a->b,c,d  b->e,f c->^ d->f,g g->h
+
+#include<stdio.h>
+
+struct student
+{
+	char a[3];
+	stu1 Stu1;
+
+};
+
+typedef struct student1
+{
+	char b[1];
+	char c[1] = { NULL };
+	char d[2];
+	stu2 Stu2;
+	
+}stu1;
+
+typedef struct student2
+{
+	char e;
+	char f;
+	char g
+}stu2;
+
+typedef 
+
