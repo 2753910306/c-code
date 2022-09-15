0b56c0f1ab3266791713cc7ae9c80c274eff444c
2753910306 <87286181+2753910306@users.noreply.github.com>
2022-09-15 18:10:26 +0800

    Add files via upload

---
+++ 2.5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << 3.5 * 3 + 2 * 7 - 'A'<<'\n';
+	cout << 26 / 3 + 34 % 3 + 2.5 << '\n';
+	cout << 45 / 2 + (int)3.14159 / 2 << '\n';
+
+	return 0;
+}
