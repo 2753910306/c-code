48b9c7fd4a105e5dd6552b9ecf32c6207449ecd2
2753910306 <87286181+2753910306@users.noreply.github.com>
2022-09-18 08:16:20 +0800

    Add files via upload

---
+++ 3.4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char c1, c2;
+	c1 = getchar();
+	c2 = getchar();
+	putchar(c1);
+	putchar(c2);
+	cout << endl;
+	cout << c1 + 0 << endl;
+	cout << c2 + 0 << endl;
+	return 0;
+}
